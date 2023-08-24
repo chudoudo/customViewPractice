02e3470e87db8984505aec1032ec492f38145f49
chudoudo <fraumarch@gmail.com>
2023-08-24 09:25:24 +0200

    making the model of the rocket details for the main layout

--- app/src/main/java/com/example/customviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/customviewpractice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.customviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customviewpractice.databinding.ActivityMainBinding
+import com.example.customviewpractice.view.RocketDetailView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
